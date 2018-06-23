d3bee9698ac45e89c1c598890d10d57dafc1aefc
amiroshnichenko <a.miroshnichenko@hh.ru>
2018-06-23 11:13:42 +0300

    Initial commit

---
+++ app/src/main/java/me/lazarte/tony/slimming/AppRootActivity.kt
@@ -0,0 +1,12 @@
+package me.lazarte.tony.slimming
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AppRootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_root)
+    }
+}
